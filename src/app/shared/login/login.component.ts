dd78d4853c03e1354626e2c7bc3daa2af122da9f
Rahul Kumar <rahul.kumar.x@venturepact.com>
2017-07-24 12:10:10 +0530

    Changes

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,9 +1,8 @@
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { EmailValidator } from './../validators/email.validator';
-import { environment } from './../../../environments/environment';
-import { User, Email} from  './user';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {EmailValidator} from './../validators/email.validator';
+import {Email, User} from './user';
 import {Script} from '../services/script.service';
 import {Title} from '@angular/platform-browser';
 import {MembershipService} from '../services/membership.service';
@@ -16,44 +15,43 @@ import {UserService} from '../services/user.service';
 declare var jQuery: any;
 
 @Component({
-	selector: 'og-login',
-	templateUrl: './login.component.html',
+  selector: 'og-login',
+  templateUrl: './login.component.html',
   encapsulation: ViewEncapsulation.None,
-	styleUrls: ['./login.component.css', './../../../assets/css/sahil-hover.css', './../../../assets/css/custom-material.css'],
+  styleUrls: ['./login.component.css', './../../../assets/css/sahil-hover.css', './../../../assets/css/custom-material.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   error: Boolean = false;
-  isError: Boolean = false;
   co: any;
   ErrorMsg: String;
+  isError: boolean;
   userId: any;
   isDomainExist: Boolean = false;
   resendEmailShow: Boolean = false;
   model = new User(new Email('', true), '');
 
   isAdminCreated: boolean = false;
-  cardStatus:string = '';
+  cardStatus: string = '';
 
-  constructor(
-    public fb: FormBuilder,
-    public _userService: UserService,
-    public router: Router,
-    public loggedInSerivce: LoggedInService,
-    public subDomainService : SubDomainService,
-    public _companyService : CompanyService,
-    public _cookieService : CookieService,
-    public _membershipService:MembershipService,
-    public _script: Script,
-    public titleService: Title,
-    public route: ActivatedRoute) {
-      this.route.queryParams.subscribe((params : Params) => {
-        if(this.validateEmail(params['email'])){
-          this.model.emails.email = params['email'];
-        }
-      });
-      this.titleService.setTitle("Outgrow Home");
-    }
+  constructor(public fb: FormBuilder,
+              public _userService: UserService,
+              public router: Router,
+              public loggedInSerivce: LoggedInService,
+              public subDomainService: SubDomainService,
+              public _companyService: CompanyService,
+              public _cookieService: CookieService,
+              public _membershipService: MembershipService,
+              public _script: Script,
+              public titleService: Title,
+              public route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (this.validateEmail(params['email'])) {
+        this.model.emails.email = params['email'];
+      }
+    });
+    this.titleService.setTitle("Outgrow Home");
+  }
 
   ngOnInit() {
     let link = window.location.hostname.split('.');
@@ -69,13 +67,13 @@ export class LoginComponent implements OnInit {
         Validators.required, EmailValidator.format
       ])],
       password: [this.model.password, Validators.compose([
-        Validators.required,Validators.minLength(8)
+        Validators.required, Validators.minLength(8)
       ])]
     });
     jQuery.material.init();
   }
 
-  validateEmail(email:any) {
+  validateEmail(email: any) {
     let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
@@ -98,90 +96,55 @@ export class LoginComponent implements OnInit {
     let companyName: String = null;
     if (linkArray.length === 3 && linkArray[0] !== 'app')
       companyName = linkArray[0];
+
     this._userService.login(value.email, value.password, companyName)
       .subscribe((response: any) => {
-        //console.log('filepicker_token_set', response.companyAccess);
-        if (response.token) {
-          self.isAdminCreated = response.user.is_admin_created;
-          if(response.user.role !== 'ADMIN'){
-            self.cardStatus = response.subscription.currentplan.customer.card_status;
-            localStorage.removeItem('leads');
-            response.company['cost'] = response.plan.price;
-            this.subDomainService.setCurrentCompany(response.company);
-          }
-          let storage: any;
-          if (response.user.role === 'ADMIN') {
-            storage = {
-              'token': response.token,
-              'user': response.user
-            };
-          } else {
-            storage = {
-              'token': response.token,
-              'user': response.user,
-              'currentCompany': response.company.sub_domain,
-              'companyList': response.companyList,
-              'showUpgradeModal': false,
-            };
-            // storage.company['cost'] = response.plan.price;
-            // console.log('STORAGE JSON', storage.company.cost);
-            self._cookieService.createCookie('filepicker_token_json', JSON.stringify(response.companyAccess), 3);
-            if (response.user.chargebee_plan_id === 'starter')
-              storage.showUpgradeModal = true;
-          }
-          jQuery('#login').modal('hide');
-          jQuery('#is-Error').addClass('hide');
-          self._cookieService.createCookie('storage', JSON.stringify(storage), 3);
-          if(response.user.role !== 'ADMIN'){
-            let status = {
-              cardStatus: self.cardStatus,
-              subsStatus: response.subscription.currentplan.subscription.status
-            };
-            self._cookieService.createCookie('status', JSON.stringify(status),3);
-          }
-          if (response.user.role !== 'ADMIN') {
-            self.loggedInSerivce.login();
-            self._userService.token = response.token;
-            let url = response.company.sub_domain + '.' + environment.APP_EXTENSION + '/dashboard';
-            if (!self.subDomainService.subDomain.is_sub_domain_url) {
-              jQuery(location).attr('href', environment.PROTOCOL + url);
+          if (response.token) {
+            let storage: any;
+            if (response.user.role === 'ADMIN') {
+              storage = {
+                'token': response.token,
+                'user': response.user
+              };
             } else {
-              window.location.href = window.location.origin + '/dashboard';
+              self.ErrorMsg = 'Please Enter Correct admin credentials';
+              jQuery('#is-Error').addClass('show');
+            }
+            jQuery('#loginSubmit').removeClass('loading');
+            jQuery('#loginSubmit').html('Login');
+            jQuery('#loginSubmit').attr('disabled', false);
+            jQuery('#loginSubmit').attr('disabled', false);
+            self._cookieService.createCookie('storage', JSON.stringify(storage), 3);
+            if (response.user.role === 'ADMIN') {
+              window.location.href = window.location.origin + '/admin/companies';
+            }
+            else {
             }
           }
-          else {
-            window.location.href = window.location.origin + '/admin/companies';
-          }
-        }
-      },
-      (response: any) => {
-        self.resendEmailShow = false;
-        jQuery('#loginSubmit').removeClass('loading');
-        jQuery('#loginSubmit').html('Login');
-        jQuery('#loginSubmit').attr('disabled', false);
-        jQuery('#loginSubmit').attr('disabled', false);
-        jQuery('#is-Error').removeClass('hide');
-        //self.isError = true;
-        self.ErrorMsg = response.error.message;
-        if (response.error.code === 'E_USER_NOT_FOUND') {
-          self.ErrorMsg = "Something isn't quite right. Please enter a correct email and password..";
-        }
-        else if (response.error.code === 'E_USER_ACCOUNT_DISABLED') {
-          self.userId = response.error.err_errors;
-          // if (self.userId !== null) {
-          //   self.ErrorMessage = 'Your account has been disabled. Please verify your email to continue using Outgrow.';
-          //   // self.resendEmailShow = true;
-          // }else {
-            self.ErrorMsg = 'Your account has been disabled. Please signup again!';
-          // }
-        }
-        else if (response.error.code === 'E_USER_ACCOUNT_LEFT') {
+        },
+        (response: any) => {
+          self.resendEmailShow = false;
+          jQuery('#loginSubmit').removeClass('loading');
+          jQuery('#loginSubmit').html('Login');
+          jQuery('#loginSubmit').attr('disabled', false);
+          jQuery('#loginSubmit').attr('disabled', false);
+          jQuery('#is-Error').removeClass('hide');
+          //self.isError = true;
           self.ErrorMsg = response.error.message;
-          self.userId = response.error.err_errors;
+          if (response.error.code === 'E_USER_NOT_FOUND') {
+            self.ErrorMsg = "Something isn't quite right. Please enter a correct email and password..";
+          }
+          else if (response.error.code === 'E_USER_ACCOUNT_DISABLED') {
+            self.userId = response.error.err_errors;
+            self.ErrorMsg = 'Your account has been disabled. Please signup again!';
+          }
+          else if (response.error.code === 'E_USER_ACCOUNT_LEFT') {
+            self.ErrorMsg = response.error.message;
+            self.userId = response.error.err_errors;
+          }
+          console.log("error message", self.ErrorMsg);
+          jQuery("#login-error-msg").html(self.ErrorMsg);
         }
-        console.log("error message",self.ErrorMsg);
-        jQuery("#login-error-msg").html(self.ErrorMsg);
-      }
       );
   }
 
@@ -192,7 +155,7 @@ export class LoginComponent implements OnInit {
   }
 
   closeLogin() {
-      // Redirecting to main home page //
+    // Redirecting to main home page //
     this.router.navigate(['/']);
   }
 
