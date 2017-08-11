df94a8143d7025923ae6cadbd49394d2da8f92e8
biplabmalakar <biplab.x@venturepact.com>
2017-08-11 16:41:44 +0530

    some changes

--- src/app/admin/londoner/londoner.component.ts
+++ src/app/admin/londoner/londoner.component.ts
@@ -20,6 +20,7 @@ export class LondonerComponent extends Datatable implements OnInit {
   ngOnInit() {
     this._http.getSoundCloud()
       .subscribe(data => {
+        console.log('Data:  ', data);
         for (let d of data) {
           if (d.company) {
             this.calculator.push(d);
@@ -33,14 +34,16 @@ export class LondonerComponent extends Datatable implements OnInit {
   }
 
   saveTime(i) {
-    let id = this.calculator[i]._id;
-    let timeVal = parseFloat(this.timeData[i]);
-    this._http.updateTime({ id, timeVal })
-      .subscribe(data => alert('Time Saved'), err => { alert('Not Saved'); console.log('Error:  ', err) })
+    if (this.timeData[i]) {
+      let id = this.calculator[i]._id;
+      let timeVal = parseFloat(this.timeData[i]);
+      this._http.updateTime({ id, timeVal })
+        .subscribe(data => alert('Time Saved'), err => { alert('Not Saved'); console.log('Error:  ', err) })
+    }
   }
 
   searchData() {
-    
+
     if (this.search == '') {
       console.log('Running e')
       this.timeData = [];
@@ -56,7 +59,7 @@ export class LondonerComponent extends Datatable implements OnInit {
       for (let d of this.calculatorData) {
         if (d.name.substr(0, this.search.length) == this.search || d.company.sub_domain.substr(0, this.search.length) == this.search) {
           this.calculator.push(d);
-         (typeof d.time_to_email != 'undefined') ? this.timeData.push(d.time_to_email) : this.timeData.push(0);
+          (typeof d.time_to_email != 'undefined') ? this.timeData.push(d.time_to_email) : this.timeData.push(0);
         }
       }
     }
