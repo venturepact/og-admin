1896aaeda6da9546d0ead4029a737b5e765d64c5
ajit-jain <jainajit194@gmail.com>
2017-12-29 11:05:34 +0530

    changes

--- src/app/admin/companies/single-company/components/company-premades/company-premades.component.ts
+++ src/app/admin/companies/single-company/components/company-premades/company-premades.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CompanyService } from '../../../../../shared/services/index';
 import { AdminService } from '../../../../../shared/services/admin.service';
-
+declare var jQuery:any;
 @Component({
   selector: 'company-premades',
   templateUrl: './company-premades.component.html',
@@ -13,18 +13,27 @@ export class CompanyPremadesComponent implements OnInit {
   loading:boolean=true;
   data:any;
   templates=[];
+  availableTemplates=[];
+
   constructor(private companyService:CompanyService,private adminService:AdminService) {
    }
 
   ngOnInit() {
     this.getAvailableTemplates();
+
+    this.companyService.getCompanyTemplates().subscribe(data=>{
+      this.availableTemplates=data;     
+      this.disableTemplates(); 
+    })
   }
   getCompanyPremades(){
     this.loading=true;
     this.companyService.getCompanyPremades(this.company)
     .subscribe((data)=>{
-      console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",data);
       this.data = data;
+      this.templates.forEach((obj,index)=>{
+        this.checkForParentStatus(index,obj.selector);
+      })
       this.loading=false;
     })
   }
@@ -32,10 +41,8 @@ export class CompanyPremadesComponent implements OnInit {
     this.getCompanyPremades();    
   }
   updateCompanyPremades(){
-    console.log(">>>>>>>>>>>>>>>>>",this.data);
     this.companyService.updateCompanyPremades({company:this.company,premades:this.data.premades})
     .subscribe((data)=>{
-      console.log(">>>>>>>>>>>>>>>>>>>",data);
       this.getCompanyPremades();
     })
   }
@@ -45,4 +52,38 @@ export class CompanyPremadesComponent implements OnInit {
     }) 
   }
 
+  selectAll(value,selector){
+    this.data && this.data.premades.forEach(element => {
+        if(element.template == selector)
+          element.active = value
+    });
+  }
+  checkForParentStatus(parentIndex,selector){
+    let filteredArray = this.data.premades.filter(obj=>{
+      return (obj.template == selector);
+    });
+    if(filteredArray.length == 0){
+      this.templates[parentIndex].available=false;
+      return;
+    }
+    let result = filteredArray.every(obj=>{
+      return obj.active
+    })
+    this.templates[parentIndex].available=result;
+  }
+  disableTemplates(){
+    if(this.templates.length > 0){
+      this.availableTemplates.forEach((obj)=>{
+        let index = this.templates.findIndex(ele=> (ele.name == obj.feature.name));
+        if(!obj.active){
+          setTimeout(()=> {
+            jQuery("#template"+index+" :input").attr("disabled", true);
+          }, 500);
+        }else{
+          jQuery("#template"+index+" :input").attr("disabled", false);
+        } 
+      })
+    }
+  }
+  
 }
--- src/app/admin/plans/premade-calcs/premade-calcs.component.ts
+++ src/app/admin/plans/premade-calcs/premade-calcs.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { AdminService } from './../../../shared/services/admin.service';
 import { PlanService } from './../../../shared/services/plan.service';
 import { Component, OnInit,ViewEncapsulation} from '@angular/core';
@@ -13,15 +14,13 @@ import { SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 export class PremadeCalcsComponent implements OnInit {
   @Input() plan:any;
   loading:Boolean=true;
-  premades_calcs=[];
+  premades_calcs:any=[];
   templates=[];
   constructor(private planService:PlanService,
     private adminService:AdminService) { }
 
-  ngOnInit() {
-      this.getAvailableTemplates();
-  }
-  ngOnChanges(changes:SimpleChanges){
+  ngOnInit() { }
+  ngOnChanges(changes:SimpleChanges){ 
     this.getPlanPremades();
   }
   getPlanPremades(){
@@ -29,6 +28,7 @@ export class PremadeCalcsComponent implements OnInit {
     this.planService.getPlanPremades(this.plan._id).subscribe((data)=>{
       this.loading=false;
       this.premades_calcs=data;
+      this.getAvailableTemplates();
     });
   }
   updateCalcs(){
@@ -39,8 +39,30 @@ export class PremadeCalcsComponent implements OnInit {
   }
   getAvailableTemplates(){
     this.adminService.getAvailableTemplates().subscribe((data)=>{
+      console.log("getPremades",data);
       this.templates=data;
+      this.templates.forEach((obj,index)=>{
+        this.checkForParentStatus(index,obj.selector);
+      })
     }) 
   }
-
+  checkForParentStatus(parentIndex,selector){
+    let filteredArray = this.premades_calcs.premades.filter(obj=>{
+      return (obj.template == selector);
+    });
+    if(filteredArray.length == 0){
+      this.templates[parentIndex].available=false;
+      return;
+    }
+    let result = filteredArray.every(obj=>{
+      return obj.active
+    })
+    this.templates[parentIndex].available=result;
+  }
+  selectAll(value,selector){
+    this.premades_calcs && this.premades_calcs.premades.forEach(element => {
+        if(element.template == selector)
+          element.active = value
+    });
+  }
 }
