87bcae5cc64dfc4365fba123bc38b1eadc3e64ba
Rahul kumar <rahul.kumar@venturepact.com>
2018-05-31 16:05:46 +0530

    remove env files

--- environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    API: 'https://outgrow-api.herokuapp.com/api/v1',
-    APP_DOMAIN: 'http://admin.rely.co',
-    APP_EXTENSION: 'rely.co',
-    PARENT_APP_DOMAIN: 'admin.rely.co',
-    PROTOCOL: 'http://',
-    LIVE_EXTENSION: 'rely.co',    
-};
