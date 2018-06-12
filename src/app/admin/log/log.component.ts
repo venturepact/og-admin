67b16eac38560fe40be82d8706f04aed5d70b78e
abhishek-mittal <abhishek.mittal@venturepact.com>
2018-06-12 12:47:38 +0530

    few changes in logs

--- src/app/admin/log/log.component.ts
+++ src/app/admin/log/log.component.ts
@@ -1,107 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { AdminService } from '../../shared/services/admin.service';
-// import { Router } from "@angular/router";
-// import { CookieService, Script } from "../../shared/services";
-// import { Datatable } from '../../shared/interfaces/datatable.interface';
-
-// declare var jQuery: any;
-
-// @Component({
-//   selector: 'error-log',
-//   templateUrl: './log.component.html',
-//   styleUrls: ['./log.component.css'
-//     , '../../site/components/+analytics/assets/css/daterangepicker.css',
-//     './../search-calc/search-calc.component.css']
-// })
-// export class LogComponent extends Datatable implements AfterViewInit, OnInit {
-//   scriptLoaded = false;
-//   logs: Array<String>;
-//   message: String;
-//   folderName: String;
-//   dateme: string;
-//   firsTime = true;
-//   isLoading = false;
-
-//   constructor(public _script: Script, public _adminService: AdminService,
-//     public _cookieService: CookieService, public router: Router) {
-//     super();
-//     if (this._cookieService.readCookie('storage')) {
-//       let storage = JSON.parse(this._cookieService.readCookie('storage'));
-//       if (storage.user.sub_role !== null)
-//         this.router.navigate(['/admin/users']);
-//     }
-//   }
-
-//   ngOnInit() {
-//     this._adminService.getlogType().subscribe((data) => {
-//       this.folderName = data;
-//       let dateObj = new Date();
-//       let dateString = this.formateDate(dateObj);
-//       console.log(dateString);
-//       this.requestForLog(dateString, this.folderName);
-//     });
-
-//     let $cont = jQuery('#logContainer');
-//     console.log($cont[0]);
-//     $cont[0].scrollTop = $cont[0].scrollHeight;
-//   }
-
-//   ngAfterViewInit() {
-//     this._script.load('daterangepicker', 'datatables')
-//       .then((data) => {
-//         this.scriptLoaded = true;
-//         // this.requestForLog(this.dateme, this.folderName);
-//       })
-//       .catch((error) => {
-//         console.log('Script not loaded', error);
-//       });
-//   }
-
-//   requestForLog(dateString, folder) {
-//     let obj = {
-//       limit: this.current_limit,
-//       page: this.current_page - 1,
-//       // searchKey: this.search,
-//       selectedDate: dateString,
-//       folder: folder,
-//       isFirstTime: this.firsTime
-//     };
-//     this._adminService.getLog({ selectedDate: dateString, folder: folder }).subscribe((response) => {
-//       console.log(response);
-//       this.logs = response;
-//       this.message = '';
-//     }, (response) => {
-//       this.message = 'No log found!!';
-//       this.logs = [];
-//     });
-//   }
-
-//   formateDate(d) {
-//     let month = '' + (d.getMonth() + 1),
-//       day = '' + d.getDate(),
-//       year = d.getFullYear();
-
-//     if (month.length < 2) month = '0' + month;
-//     if (day.length < 2) day = '0' + day;
-
-//     return [year, month, day].join('-');
-//   }
-
-//   onDateSelect(date: any) {
-//     console.log(date);
-//     //this.$getErrorList= this._adminService.getLog({selectedDate:date.start_date,folder:this.folderName});
-//     this.requestForLog(date.start_date, this.folderName);
-//   }
-
-//   onRefresh() {
-//     let dateObj = new Date();
-//     let dateString = this.formateDate(dateObj);
-//     console.log(dateString);
-//     this.requestForLog(dateString, this.folderName);
-//   }
-// }
-
-
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AdminService } from '../../shared/services/admin.service';
 import { Router } from "@angular/router";
@@ -125,7 +21,7 @@ export class LogComponent extends Datatable {
   dateme: string;
   firsTime = true;
   isLoading = false;
-  search: string;
+  searchQuery: string;
   searchON = true;
 
   constructor(public _script: Script, public _adminService: AdminService,
@@ -179,7 +75,7 @@ export class LogComponent extends Datatable {
       // searchKey: this.search,
       selectedDate: dateString,
       folder: folder,
-      searchKey: this.search,
+      searchKey: this.searchQuery,
       isFirstTime: this.firsTime
     };
     this.isLoading = true;
@@ -215,6 +111,7 @@ export class LogComponent extends Datatable {
   onDateSelect(date: any) {
     console.log(jQuery('.input-daterange-datepicker'));
     this.firsTime = true;
+    this.searchQuery = "";
     console.log(date);
     this.reset();
     //this.$getErrorList= this._adminService.getLog({selectedDate:date.start_date,folder:this.folderName});
@@ -224,6 +121,7 @@ export class LogComponent extends Datatable {
 
   onRefresh() {
     this.firsTime = true;
+    this.searchQuery = "";
     let dateObj = new Date();
     this.reset();
     let dateString = this.formateDate(dateObj);
@@ -258,7 +156,7 @@ export class LogComponent extends Datatable {
   }
 
   searchData() {
-    if(this.search.length > 2 || this.search.length === 0){
+    if(this.searchQuery || this.searchQuery.length > 2 || this.searchQuery.length === 0){
       super.searchData();
       this.firsTime = false;
       this.requestForLog(this.dateme, this.folderName);
