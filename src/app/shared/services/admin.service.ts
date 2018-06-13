8beafd85f0efeebaf49310b076ba93fb18ddf0f9
abhishek-mittal <abhishek.mittal@venturepact.com>
2018-06-13 11:28:17 +0530

    Feature ADD: API Select in Error Logs

--- src/app/shared/services/admin.service.ts
+++ src/app/shared/services/admin.service.ts
@@ -8,6 +8,7 @@ import { BaseService } from "./base.service";
 import { ReplaySubject } from "rxjs/ReplaySubject";
 import { Observable } from "rxjs/Observable";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { APIBasedServiceProvider } from './helper-service/api-based-request';
 
 @Injectable()
 export class AdminService extends BaseService {
@@ -240,12 +241,14 @@ export class AdminService extends BaseService {
       .catch(this.handleError);
   }
 
-  getLog(data): Observable<any> {
-
-    return this._http.post(this._url + '/admin/getLog', data, this.options)
+  getLog( source_api = 'default', data): Observable<any> {
+    const url = APIBasedServiceProvider.APISwitch(source_api);
+    return this._http.post( url + '/admin/getLog', data, this.options)
       .map(this.extractData)
       .catch(this.handleError);
   }
+
+
 
   notifylogType(type) {
     this.getLogSubject.next(type);
